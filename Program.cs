49c25cf68081a7788276e225c36e4814a4f1b975
Oğuz Evrensel <oevrensel@gmail.com>
2024-12-22 15:14:49 +0300

    Commit 1 Added to program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, From Main Branch!");
 
+
 Console.WriteLine("Commit 1 From Feature B Branch!");
 
 Console.WriteLine("Commit 2 From Feature B Branch!");
+
+Console.WriteLine("Commit 1 From Feature A Branch!");
